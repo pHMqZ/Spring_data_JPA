82ca8f379a4d6095d9a4b408f7d0475b62b44ca3
Phillip Marques <phillip.silva@capgemini.com>
2023-02-25 18:15:13 -0300

    Implementing CRUD for physical evaluations and students.

--- src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
@@ -1,12 +1,45 @@
 package me.dio.academia.digital.entity.form;
 
-import me.dio.academia.digital.entity.Aluno;
-
 public class AvaliacaoFisicaForm {
 
-  private Long alunoId;
+	private Long alunoId;
 
-  private double peso;
+	private double peso;
 
-  private double altura;
+	private double altura;
+
+	public Long getAlunoId() {
+		return alunoId;
+	}
+
+	public void setAlunoId(Long alunoId) {
+		this.alunoId = alunoId;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+
+	public AvaliacaoFisicaForm(Long alunoId, double peso, double altura) {
+		this.alunoId = alunoId;
+		this.peso = peso;
+		this.altura = altura;
+	}
+
+	public AvaliacaoFisicaForm() {
+
+	}
+
 }
